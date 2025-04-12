b505ea02aa3bee2147dd193f7fc6118353e5952a
JamilyGSS <salesjamily0@gmail.com>
2025-04-12 19:39:39 -0300

    desafios 12 e 13

---
+++ desafio12.py
@@ -0,0 +1,3 @@
+p = float(input('Qual o preço? '))
+np = (95*p)/100
+print('O valor do produto com o desconto é R${:.2f}'.format(np))
---
+++ desafio13.py
@@ -0,0 +1,3 @@
+s = float(input('Qual seu salário atual: '))
+ns = (115*s)/100
+print('Seu salário com 15% de aulmento será de R${:.2f}'.format(ns))
