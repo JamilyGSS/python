e2ca0bdc16301d1675a61167061a5aa8e7e72815
JamilyGSS <salesjamily0@gmail.com>
2025-04-13 12:54:13 -0300

    Create desafio016.py

---
+++ desafios/desafio016.py
@@ -0,0 +1,7 @@
+# import math
+# num = float(input('Digite um valor: '))
+# print('O número {} tem a parte inteiro {}'.format(num, math.trunc(num)))
+
+from math import trunc
+num = float(input('Digite um valor: '))
+print('O núemro {} tem a parte inteira {}'.format(num, trunc(num)))
