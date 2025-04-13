ed224b3652034c3a966f1c6ed2dc5e89e30e5a28
JamilyGSS <salesjamily0@gmail.com>
2025-04-13 17:21:32 -0300

    desafios 26 e 27

---
+++ desafios/desafio026.py
@@ -0,0 +1,4 @@
+frase = input('Frase: ')
+print('Quantidade de vezes que a letra A aparece: {}'.format(frase.upper().count('A')))
+print('Posição em que A aparece pela primeira vez: {}'.format(frase.upper().find('A')))
+print('Posição em que A aparece pela última vez: {}'.format(frase.upper().rfind('A')))
---
+++ desafios/desafio027.py
@@ -0,0 +1,4 @@
+nome = input('Digite seu nome: ')
+dividida = nome.split()
+print('Primeiro nome: {}'.format(dividida[0]))
+print('Último nome: {}'.format(dividida[-1]))
