c9a4509cd11d01a332911fd0a411054b0084bc87
JamilyGSS <salesjamily0@gmail.com>
2025-04-13 12:46:39 -0300

    Create aula08a.py

---
+++ aulas/aula08a.py
@@ -0,0 +1,18 @@
+# import math
+# num = int(input('Digite um número: '))
+# raiz = math.sqrt(num)
+# print('A raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
+
+# from math import sqrt, ceil
+# num = int(input('Digite um número: '))
+# raiz = sqrt(num)
+# print('A raiz de {} é igual a {}'.format(num, ceil(raiz)))
+
+# import random
+# num = random.random()
+# ou num = random.randint(1,10)
+# print(num)
+
+import emoji
+print(emoji.emojize('Python is :thumbs_up:'))
+print(emoji.emojize("Python é :mãos_de_coração:", language='pt'))
