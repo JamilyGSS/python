48d45cad329dd82ba371774d9889d1bbdba00a30
JamilyGSS <salesjamily0@gmail.com>
2025-04-18 12:05:39 -0300

    Create desafio035.py

---
+++ desafios/desafio035.py
@@ -0,0 +1,14 @@
+l1 = float(input('Digite o valor do lado 1: '))
+l2 = float(input('Digite o valor do lado 2: '))
+l3 = float(input('Digite o valor do lado 3: '))
+
+if l1 >= l2 + l3:
+    print('Não é possível formar um trângulo!')
+else:
+    if l2 >= l1 + l3:
+        print('Não é possível formar um trângulo!')
+    else:
+        if l3 >= l1 + l2:
+            print('Não é possível formar um trângulo!')
+        else:
+            print('É possível formar um trângulo!')
